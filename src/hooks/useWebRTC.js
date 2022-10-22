28cae83834d8b33c8ea3188366a4fd9bb68564c8
Allori <62593567+Allorion@users.noreply.github.com>
2022-10-22 13:11:27 +0300

    готовый билд для видеоконференции

--- src/hooks/useWebRTC.js
+++ src/hooks/useWebRTC.js
@@ -2,6 +2,7 @@ import useStateWithCallback from "./useStateWithCallback";
 import {useCallback, useEffect, useRef} from "react";
 import ACTIONS from "../socket/actions";
 import socket from "../socket";
+import freeice from "freeice";
 
 export const LOCAL_VIDEO = 'LOCAL_VIDEO';
 
@@ -24,6 +25,133 @@ export default function useWebRTC(roomID) {
     const peerMediaElements = useRef({
         [LOCAL_VIDEO]: null,
     });
+
+    useEffect(() => {
+        async function handleNewPeer({peerID, createOffer}) {
+            if (peerID in peerConnections.current) {
+                return console.warn(`Already connected to peer ${peerID}`);
+            }
+
+            peerConnections.current[peerID] = new RTCPeerConnection({
+                iceServers: freeice(),
+            });
+
+            peerConnections.current[peerID].onicecandidate = event => {
+                if (event.candidate) {
+                    socket.emit(ACTIONS.RELAY_ICE, {
+                        peerID,
+                        iceCandidate: event.candidate,
+                    });
+                }
+            }
+
+            let tracksNumber = 0;
+            peerConnections.current[peerID].ontrack = ({streams: [remoteStream]}) => {
+                tracksNumber++
+
+                if (tracksNumber === 2) { // video & audio tracks received
+                    tracksNumber = 0;
+                    addNewClient(peerID, () => {
+                        if (peerMediaElements.current[peerID]) {
+                            peerMediaElements.current[peerID].srcObject = remoteStream;
+                        } else {
+                            // FIX LONG RENDER IN CASE OF MANY CLIENTS
+                            let settled = false;
+                            const interval = setInterval(() => {
+                                if (peerMediaElements.current[peerID]) {
+                                    peerMediaElements.current[peerID].srcObject = remoteStream;
+                                    settled = true;
+                                }
+
+                                if (settled) {
+                                    clearInterval(interval);
+                                }
+                            }, 1000);
+                        }
+                    });
+                }
+            }
+
+            localMediaStream.current.getTracks().forEach(track => {
+                peerConnections.current[peerID].addTrack(track, localMediaStream.current);
+            });
+
+            if (createOffer) {
+                const offer = await peerConnections.current[peerID].createOffer();
+
+                await peerConnections.current[peerID].setLocalDescription(offer);
+
+                socket.emit(ACTIONS.RELAY_SDP, {
+                    peerID,
+                    sessionDescription: offer,
+                });
+            }
+        }
+
+        socket.on(ACTIONS.ADD_PEER, handleNewPeer);
+
+        return () => {
+            socket.off(ACTIONS.ADD_PEER);
+        }
+
+    }, [])
+
+
+    useEffect(() => {
+        async function setRemoteMedia({peerID, sessionDescription: remoteDescription}) {
+            await peerConnections.current[peerID]?.setRemoteDescription(
+                new RTCSessionDescription(remoteDescription)
+            );
+
+            if (remoteDescription.type === 'offer') {
+                const answer = await peerConnections.current[peerID].createAnswer();
+
+                await peerConnections.current[peerID].setLocalDescription(answer);
+
+                socket.emit(ACTIONS.RELAY_SDP, {
+                    peerID,
+                    sessionDescription: answer,
+                });
+            }
+        }
+
+        socket.on(ACTIONS.SESSION_DESCRIPTION, setRemoteMedia)
+
+        return () => {
+            socket.off(ACTIONS.SESSION_DESCRIPTION);
+        }
+    }, []);
+
+    useEffect(() => {
+        socket.on(ACTIONS.ICE_CANDIDATE, ({peerID, iceCandidate}) => {
+            peerConnections.current[peerID]?.addIceCandidate(
+                new RTCIceCandidate(iceCandidate)
+            );
+        });
+
+        return () => {
+            socket.off(ACTIONS.ICE_CANDIDATE);
+        }
+    }, []);
+
+    useEffect(() => {
+        const handleRemovePeer = ({peerID}) => {
+            if (peerConnections.current[peerID]) {
+                peerConnections.current[peerID].close();
+            }
+
+            delete peerConnections.current[peerID];
+            delete peerMediaElements.current[peerID];
+
+            updateClients(list => list.filter(c => c !== peerID));
+        };
+
+        socket.on(ACTIONS.REMOVE_PEER, handleRemovePeer);
+
+        return () => {
+            socket.off(ACTIONS.REMOVE_PEER);
+        }
+    }, []);
 
     useEffect(() => {
         async function startCapture() {
