c73af3b252ac1aa1a70a7802289fa0f76fcc5b84
Allori <62593567+Allorion@users.noreply.github.com>
2022-10-22 16:54:02 +0300

    21

--- src/socket/index.js
+++ src/socket/index.js
@@ -7,6 +7,6 @@ const options = {
   transports : ["websocket"]
 }
 
-const socket = io('http://localhost:3001', options);
+const socket = io('https://vr-node-server.onrender.com', options);
 
 export default socket;
